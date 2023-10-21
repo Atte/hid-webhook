88a69fcd51de1115688e97038e97ab5fb8d45521
Atte Virtanen <aten.email@gmail.com>
2023-10-21 13:30:35 +0300

    fix nix config

--- module.nix
+++ module.nix
@@ -89,7 +89,7 @@ in
           HID_WEBHOOK_NO_VERIFY = bool cfg.no_verify;
           HID_WEBHOOK_DOWN = bool cfg.down;
           HID_WEBHOOK_UP = bool cfg.up;
-          HID_WEBHOOK_TIMEOUT = bool cfg.timeout;
+          HID_WEBHOOK_TIMEOUT = builtins.toString cfg.timeout;
           HID_WEBHOOK_IGNORE_KEYS = lib.concatStringsSep "," (builtins.map builtins.toString cfg.ignore_keys);
 
           RUST_LOG = "hid_webhook=info";
